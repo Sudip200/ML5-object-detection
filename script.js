a74606ab19101f115011f6921c7261f0ec93e742
Sudipto Das <dassudipto200@gmail.com>
2022-10-07 12:16:51 +0000

    2nd commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let video;
 let mobilnet;
 let label;
-var res=document.getElementsByTagName("h2");
-console.log('ml5 version:', ml5.version);
+const res=document.getElementById("res");
+
 function modelReady(){
     console.log("Your model is ready");
     mobilnet.predict(video,gotResult)
@@ -12,20 +12,16 @@ function gotResult(error,result){
   if(error){
     console.log(error);
   }else{
-    //console.log(result);
      label=result[0].label;
      let prob=result[0].confidence;
-
-    
-    //mobilnet=ml5.imageClassifier('MobileNet',modelReady)
-   res.innerHTML=label;
+     res.innerHTML=label
+     mobilnet=ml5.imageClassifier('MobileNet',modelReady)
   }
 }
 function setup(){
     createCanvas(600,500);
     video=createCapture(VIDEO);
     video.hide();
-   // mobilnet=ml5.imageClassifier('MobileNet',modelReady)
 }
 function startPrediction(){
   mobilnet=ml5.imageClassifier('MobileNet',modelReady)
@@ -35,7 +31,5 @@ function startPrediction(){
 function draw() {
     background(0);
     image(video,0,0);
-   fill(55);
-   textSize(64);
-   text(label,10,height-100)
-  }
+   //fill(55);
+}
